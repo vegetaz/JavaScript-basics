60fec4d6ef4cc4ddc58ff84652c5bf2bfa06f9d5
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 14:46:57 +0700

    Function (Hàm) trong javascript

---
+++ JS03-Functions/Functions16.js
@@ -0,0 +1,32 @@
+/*
+Cho 1 chương chình mẫu.
+Gọi function (hàm) sayHello để in message ra màn hình
+Đầu vào: none
+Đầu ra: print message to screen
+*/
+
+function sayHello() {
+    console.log("Hello");
+}
+
+function run() {
+    sayHello();
+}
+
+/*
+Function (Hàm) là một nhóm các câu lệnh thực hiện các nhiệm vụ cụ thể và có thể được giữ và duy trì riêng biệt tạo thành chương trình chính.
+Các hàm cung cấp một cách để tạo các gói code(mã) có thể tái sử dụng, dễ mang theo hơn và dễ gỡ lỗi hơn.
+
+Dưới đây là một số lợi thế của việc sử dụng function:
+1. Functions giảm sự lặp lại code trong chương trình
+Function cho phép bạn trích xuất khối lệnh thường được sử dụng thành một thành phần duy nhất.
+Bây giờ bạn có thể thực hiện cùng một tác vụ bằng cách gọi hàm này bất cứ nơi nào bạn muốn trong tập lệnh của mình mà không phải sao chép và dán cùng một khối lệnh nhiều lần
+2. Functions giúp các dòng code dễ bảo trì hơn
+Do một function được tạo một lần có thể được sử dụng nhiều lần, do đó, mọi thay đổi được thực hiện bên trong một function sẽ tự động được thực hiện tại tất cả các vị trí mà không cần thay đổi ở nhiều nơi.
+3. Functions giúp dễ dàng hơn để loại bỏ các lỗi
+Khi chương trình được chia thành các function, nếu có lỗi xảy ra, bạn biết chính xác function nào gây ra lỗi và tìm nó ở đâu.
+Do đó, sửa lỗi trở nên dễ dàng hơn nhiều.
+
+Khai báo và gọi 1 function
+Khởi tạo 1 function bắt đầu với từ khóa function, tiếp theo đó là tên mà function mà bạn muốn tạo, tiếp theo là ngoặc đơn () và cuối cùng là nơi cách lệnh của function giữa các dấu ngoặc nhọn {}.
+*/
