dde900e92adf6b3835516e7a04c26dc86992a192
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:07:35 +0700

    console.log()

---
+++ JS00-Overview/Overview02.js
@@ -0,0 +1,7 @@
+/*
+console.log() là một hàm trong JavaScript, được sử dụng để in bất kỳ biến nào được xác định trước đó hoặc chỉ in bất kỳ thông báo nào cần hiển thị cho người dùng.
+*/
+
+function run() {
+    console.log('Hello World');
+}
