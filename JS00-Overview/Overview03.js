aa38e76fb30db1e4857a808fd53429ce4709d59f
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:10:50 +0700

    Nối chuỗi trong javascript

---
+++ JS00-Overview/Overview03.js
@@ -0,0 +1,12 @@
+/*
+Cho 2 chuỗi, firstName và lastName, In ra 1 chuỗi mới với định dạng "firstName, lastName".
+Đầu vào: firstName và lastName
+Đầu ra: In ra 1 chuỗi mới với định dạng "firstName, lastName".
+*/
+
+function run() {
+    var firstName = "Tuan";
+    var lastName = "Phong";
+    var name = firstName + ", " + lastName; // Toán tử + đươc sử dụng cho việc nối chuỗi trong javascript nếu ít nhất 1 trong các chuỗi là chuỗi.
+    console.log(name);
+}
