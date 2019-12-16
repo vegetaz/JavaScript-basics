d28ccda9bea5d6b3673f7491b5c3b5b113dbe586
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:03:58 +0700

    Overview

---
+++ JS00-Overview/Overview01.js
@@ -0,0 +1,37 @@
+function run() {
+    console.log('Hello World');
+}
+
+/*
+JavaScript là gì
+JavaScript là ngôn ngữ kịch bản phía máy khách phổ biến nhất và được sử dụng rộng rãi.
+Kịch bản phía máy khách đề cập đến các tập lệnh chạy trong trình duyệt web của bạn.
+JavaScript được thiết kế để thêm hiệu ứng tương tác và động cho các trang web bằng cách thao tác nội dung được trả về từ máy chủ web.
+
+JavaScript là một ngôn ngữ hướng đối tượng và nó cũng có một số điểm tương đồng về cú pháp với ngôn ngữ lập trình Java.
+Nhưng, JavaScript không liên quan đến Java theo bất kỳ cách nào.
+
+Tại sao cần học JS
+JavaScript là 1 trong 3 ngôn ngữ mà tất cả các nhà phát triển website phải học:
+   1. HTML để xây dựng nội dung của website
+   2. CSS để chỉ định bố cục của các trang web
+   3. JavaScript để lập trình hành vi của các trang web
+
+Các trang web không phải là nơi duy nhất sử dụng JavaScript.
+Nhiều chương trình máy tính để bàn và máy chủ sử dụng JavaScript. Node.js là nổi tiếng nhất.
+Một số cơ sở dữ liệu, như MongoDB và CouchDB, cũng sử dụng JavaScript làm ngôn ngữ lập trình của họ.
+
+Bạn có thể làm gì với javascript
+Có rất nhiều điều bạn có thể làm với JavaScript.
+
+Bạn có thể sửa đổi nội dung của một trang web bằng cách thêm hoặc xóa các yếu tố.
+Bạn có thể thay đổi kiểu dáng và vị trí của các thành phần trên trang web.
+Bạn có thể theo dõi các sự kiện như nhấp chuột, di chuột, v.v. và phản ứng với nó.
+Bạn có thể thực hiện và kiểm soát chuyển tiếp và hình ảnh động.
+Bạn có thể tạo cửa sổ bật lên cảnh báo để hiển thị thông tin hoặc thông báo cảnh báo cho người dùng.
+Bạn có thể thực hiện các thao tác dựa trên đầu vào của người dùng và hiển thị kết quả.
+Bạn có thể xác nhận đầu vào của người dùng trước khi gửi nó đến máy chủ.
+
+Danh sách không kết thúc ở đây, có nhiều điều thú vị khác mà bạn có thể làm với JavaScript.
+Bạn sẽ tìm hiểu về tất cả chúng một cách chi tiết trong các chương sắp tới.
+*/
