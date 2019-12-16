88607b0ee76d1963dbfa4b1701306ff22951e04e
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 14:17:41 +0700

    Các toán tử trong JS

---
+++ JS02-Operators/Operators10.js
@@ -0,0 +1,34 @@
+/*
+Tìm 2 số khi biết tổng và hiều.
+Cho 2 số là tổng và hiệu của 2 số a và b.
+Viết function tính toán a và b và in ra màn hình
+Ví dụ: 
+cho  sum = 10, different = 2, Kết quả nên là
+6
+4
+Đầu vào: 2 số sum và different
+Đầu ra: a và b và in chúng ra màn hình
+*/
+
+function run(sum, different) {
+    var a = (sum + different) / 2;
+    var b = (sum - different) / 2;
+
+    console.log('a is ' + a + " and b is " + b);
+}
+
+/*
+Javascript cung cấp một tập hợp các toán tử phong phú để thao tác các biến.
+Chúng tôi có thể chia tất cả các toán tử thành các nhóm sau:
+-  Toán tử số học
+-  Toán tử quan hệ
+-  Toán tử logic
+-  Toán tử gán
+-  Toán tử typeof
+
+Trong phần này, chúng ta sẽ thảo luận về các toán tử số học được sử dụng trong các biểu thức toán học giống như cách chúng được sử dụng trong đại số.
+Toán tử số học được sử dụng để thực hiện các phép toán số học trên các toán hạng.
+Các toán tử sau được gọi là toán tử số học JavaScript.
+https://codelearn.io/LearningTask/Index?Id=136749&TaskId=126372
+
+*/
