222ed65bd9ba21a1f9e32dc1d4c9feacc3085ca5
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 14:20:27 +0700

    Toán tử nhân

---
+++ JS02-Operators/Operators11.js
@@ -0,0 +1,13 @@
+/*
+Viết chương trình với đầu vào là 1 số minutes và chuyển đổi số đó thành giây
+Ví dụ
+Cho minutes = 3; In ra 180
+Cho minutes = 10; In a 600
+Đầu vào: minutes
+Đầu ra: seconds
+*/
+
+function run(minutes) {
+    var seconds = minutes * 60;
+    return seconds;
+}
