3198e0595e40a6d7cdbc04d954dccddd94fea117
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:15:57 +0700

    comments in js

---
+++ JS00-Overview/Overview04.js
@@ -0,0 +1,39 @@
+/*
+Bài tập
+Viết vài comment cho function hiển thị "HelloWorld" ra màn hình
+
+Đầu vào: none
+Đầu ra: In "HelloWorld" Ra màn hình
+
+Gợi ý
+Đọc lý thuyết sau để biết làm thế nào để sử dụng comment trong javascript.
+
+Lý thuyết
+Mục tiêu
+JavaScript comments là cách hữu ích để truyền tải thông điệp. Nó được sử dụng để thêm thông tin vào code, cảnh báo hay gợi ý.
+Để người dùng có thể dễ dàng giải nghĩa code.
+
+Trong bài này, chúng tôi sẽ giúp bạn tạo nhưng comment trong source code
+
+Tổng quan
+The JavaScript comment được bỏ qua JavaScript engine.
+Tức là không thực thi như code javascript bình thường
+
+Có 2 kiểu comments trong JavaScript.
+
+Single-line Comment (Comment 1 dòng)
+Nó được thể hiện bằng dấu gạch chéo kép (//). Nó có thể được sử dụng trước hoặc sau 1 câu lệnh.
+
+Multi-line Comment (Comment nhiều dòng)
+Nó có thể được sử dụng để thêm comment đơn cũng như nhiều dòng.
+Vì vậy, nó là thuận tiện hơn.
+Nó được thể hiện bằng dấu gạch chéo về phía trước với dấu hoa thị rồi dấu hoa thị với dấu gạch chéo về phía trước
+*/
+
+/*
+this is function run to execute something
+*/
+function run() {
+    // show HelloWorld to screen
+    console.log("HelloWorld");
+}
