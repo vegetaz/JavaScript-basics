553cc38dde063c5939f8bd5895a9031a9d2a01e0
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:47:23 +0700

    Hằng số (const) trong JavaScript

---
+++ JS01-Variable/Variable08.js
@@ -0,0 +1,28 @@
+/*
+Khai báo 1 hằng số có giá trị là 2019 và in nó ra màn hình
+Đầu vào: none
+Đầu ra: 2019
+*/
+
+function run() {
+    const year = 2019;
+    console.log('value of constant is ' + year);
+}
+
+/*
+Hằng số trong các ngôn ngữ lập trình là một biến giá trị không thay đổi.
+Trong bài này, chúng ta sẽ học cú pháp để khai báo một hằng số trong JavaScript.
+
+Một hằng số được khai báo với từ khóa const.
+Giá trị của hằng số sẽ không thể nào thay đổi khi đã khởi tạo.
+Các biến const JavaScript phải được gán một giá trị khi chúng được khai báo.
+
+Hằng số Objects có thể thay đổi
+Bạn có thể thay đổi các thuộc tính của một hằng số object.
+Nhưng bạn ko thể gán lại một hằng số object.
+
+Hằng số mảng có thể thay đổi
+Bạn có thể thay đổi các phần tử của hằng số mảng.
+Nhưng cũng không thể gán lại hằng số mảng.
+
+*/
