d8a09fa6fe97e57542268894358d11beb07ac5c7
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:43:13 +0700

    Các quy ước đặt tên cho các biến JavaScript

---
+++ JS01-Variable/Variable07.js
@@ -0,0 +1,51 @@
+/*
+Cho 1 chương trình mẫu.
+Nhưng nó ko phải là 1 chương trình đúng.
+Các tên biến đang ko tuân theo chuẩn của JS.
+Hãy thay đổi tên biến cho chính xác và in chúng ra màn hình
+Đầu vào: Không có
+Đầu ra: Hãy thay đổi tên biến cho chính xác và in chúng ra màn hình
+*/
+
+function run() {
+    var _for = "15";
+    console.log(_for);
+    var v123 = "15";
+    console.log(v123);
+    var $break = "15";
+    console.log($break);
+    var Vreturn = 15;
+    console.log(Vreturn);
+}
+
+/*
+Các quy ước đặt tên cho các biến JavaScript
+Khi đặt tên cho các biến của bạn trong JavaScript, hãy ghi nhớ các quy tắc sau.
+- Tên biến phải bắt đầu bằng một chữ cái, gạch dưới (_), hay ký tự dollar ($).
+- Tên biến không thể bắt đầu bằng 1 chữ số.
+- Tên biến chỉ có thể chưa ký tự alpha-numeric (A-z, 0-9) và gạch dưới.
+- Tên biến không thể chưa ký tự cách.
+- Tên biến không thể là các từ khóa của JavaScript hay reserved word (từ khóa riêng).
+- Tên biến trong JS có phân biệt hoa thường. Ví dụ, Name và name là 2 biến khác nhau.
+
+Reserved Words (từ khóa riêng)
+Bảng sau là danh sách nhưng từ khóa riêng của JS.
+Chúng ko thể dc sử dụng đề đặt tên biến, hàm, phương thức, nhãn vòng lắp hay tên object.
+
+abstract	else	    instanceof  switch
+boolean	    enum	    int	        synchronized
+break	    export	    interface	this
+byte	    extends	    long	    throw
+case	    FALSE	    native	    throws
+catch	    final	    new	        transient
+char	    finally	    null	    TRUE
+class	    float	    package	    try
+const	    for	        private	    typeof
+continue	function	protected   var
+debugger	goto	    public	    void
+default	    if	        return	    volatile
+delete	    implements	short	    while
+do	        import	    static	    with
+double	    in	        super       
+
+*/
