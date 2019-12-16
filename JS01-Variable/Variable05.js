ddd2b3bdb99954f6bb9af0d9e76120cbdfb76352
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-12-16 13:25:32 +0700

    Biến và phạm vi của variable trong js

---
+++ JS01-Variable/Variable05.js
@@ -0,0 +1,31 @@
+/*
+Khai báo biến a và gán giá trị 15 cho a.
+Khai báo biến b và gán giá trị 'javascript' cho b.
+Khai báo biến c và gán giá trị true cho c.
+In ra tất cả các biến trong chương trình
+Đầu vào: Không có
+Đầu ra: In ra tất cả các biến trong chương trình
+*/
+
+function run() {
+    var a = 15;
+    var b = 'javascript';
+    var c = true;
+    console.log("a is " + a + ", b is " + b + " and c is " + c);
+}
+
+/*
+Các biến là cơ bản cho tất cả các ngôn ngữ lập trình.
+Các biến được sử dụng để lưu trữ dữ liệu, như chuỗi văn bản, số, v.v.
+Dữ liệu hoặc giá trị được lưu trữ trong các biến có thể được gán, thay đổi và truy xuất bất cứ khi nào cần.
+Nói chung, các biến là tên tượng trưng cho các giá trị.
+Bạn có thể tạo 1 biến với từ khóa var, toán tử gán (=) được sử dụng để gán giá trị cho biến, như: var varName = value;
+
+Ngoài ra, bạn cũng có thể khai báo nhiều biến và đặt giá trị ban đầu của chúng trong một câu lệnh.
+Mỗi biến được phân tách bằng dấu phẩy,.
+
+Phạm vi của một biến là khu vực của chương trình mà nó được xác định.
+Các biến JavaScript chỉ có hai phạm vi.
+- Global Variables (Biến toàn cục) − Một biến toàn cục có phạm vi toàn cầu, có nghĩa là nó có thể được định nghĩa ở bất kỳ đâu trong mã JavaScript của bạn.
+- Local Variables (Biến cục bộ) − Một biến cục bộ sẽ chỉ hiển thị trong một hàm được định nghĩa. Các tham số chức năng luôn luôn cục bộ với chức năng đó.
+*/
